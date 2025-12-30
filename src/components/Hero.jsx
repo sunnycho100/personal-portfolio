b18d397f9640bd48c2470d8bc15b3016ca2f407f
sunnycho100 <studysung1880@gmail.com>
2025-12-31 07:45:45 +0900

    v1.0.4 Hero card edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
 export default function Hero() {
   return (
     <div id="home" className="header-container section">
+      <div className="hero-photo">
+        <img src="/profile_pic.jpeg" alt="Photo of Sunny Cho" loading="lazy" />
+      </div>
+
       <div className="hero-text">
         <h1>Hello, I&apos;m Sunny!</h1>
         <p>A third-year Computer Engineering student at the University of Wisconsin–Madison.</p>
@@ -8,10 +12,6 @@ export default function Hero() {
         <a href="/Sunny_Cho_Resume.pdf" className="resume-button" download>
           <i className="fa-solid fa-file-pdf" aria-hidden="true"></i> Download Résumé
         </a>
-      </div>
-
-      <div className="hero-photo">
-        <img src="/passport_photo.JPG" alt="Photo of Sunny Cho" loading="lazy" />
       </div>
     </div>
   );
